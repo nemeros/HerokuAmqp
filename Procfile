425863ca2ff85f7b64885c66ee5d45c6187100e9
Nemeros <Nemeros>
2016-07-04 13:39:26 +0200

    rr

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web java -Dserver.port=$PORT -jar ServiceApp/target/ServiceApp-1.0.0.jar
+worker java -jar WorkerApp/target/WorkerApp-1.0.0.jar
--- WorkerApp/src/main/java/com/conf/Application.java
+++ WorkerApp/src/main/java/com/conf/Application.java
@@ -1,9 +1,25 @@
 package com.conf;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
+import com.RabbitConfig;
+
+@EnableAutoConfiguration
+@ComponentScan("com*")
+@EnableRabbit
 public class Application{
 
 	public static final void main(String[] args){
+		SpringApplication.run(Application.class, args);
+	}
 	
+	
+	@RabbitListener(queues={RabbitConfig.helloWorldQueueName})
+	public void onMessage(String data){
+		System.out.println(data);
 	}
 }
